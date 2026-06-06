484a7b026b31b7ef7a5b5527b0340e32b28b3a21
Rijad Alihodzic <alihodza77@gmail.com>
2026-06-07 00:08:02 +0200

    installing flutter and initializing starter files for frontend for desktop and mobile frontend of the app

---
+++ chargeNET/UI/chargenet_desktop/android/app/src/main/kotlin/com/chargenet/chargenet_desktop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chargenet.chargenet_desktop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ chargeNET/UI/chargenet_mobile/android/app/src/main/kotlin/com/chargenet/chargenet_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chargenet.chargenet_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
